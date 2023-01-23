ccad1025673c26764523f8520e1fc40304d85977
Kristoffer Andersson <kristoffer.andersson@gu.se>
2023-01-23 14:09:25 +0100

    test: more tests

--- karp/lex_core/tests/dict_entry_dto_unit_test.py
+++ karp/lex_core/tests/dict_entry_dto_unit_test.py
@@ -2,4 +2,14 @@ from karp.lex_core.dtos import EntryDtoDict
 
 
 def test_can_create_entry_dto():
-    _entry_dto = EntryDtoDict(entry={"field": "value"})
+    entry_dto = EntryDtoDict(entry={"field": "value"})
+
+    assert entry_dto.last_modified is None
+    assert entry_dto.last_modified_by is None
+
+
+def test_can_create_entry_dto_with_last_modified_by():
+    entry_dto = EntryDtoDict(entry={"field": "value"}, lastModifiedBy="username@example.com")
+
+    assert entry_dto.last_modified is None
+    assert entry_dto.last_modified_by == "username@example.com"
