50850ee2f425aca334e96adff74d79e32a1211b3
Kristoffer Andersson <kristoffer.andersson@gu.se>
2023-01-23 13:57:18 +0100

    fix: import

--- karp/lex_core/value_objects/unique_id.py
+++ karp/lex_core/value_objects/unique_id.py
@@ -14,7 +14,7 @@ def make_unique_id(
 ) -> UniqueId:
     """Generate an UniqueId that are sortable.
 
-    >>> from karp_lex.value_objects import make_unique_id
+    >>> from karp.lex_core.value_objects import make_unique_id
     >>> from datetime import datetime
     >>> old_id = make_unique_id(datetime(1999,12,31,23,59,59))
     >>> make_unique_id() > old_id
