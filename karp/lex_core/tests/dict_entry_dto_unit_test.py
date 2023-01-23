7e4ad70f1d0b0e045f1a854dd220e8e00cf65136
Kristoffer Andersson <kristoffer.andersson@gu.se>
2023-01-23 13:37:04 +0100

    test: fix lint

--- karp/lex_core/tests/dict_entry_dto_unit_test.py
+++ karp/lex_core/tests/dict_entry_dto_unit_test.py
@@ -2,4 +2,4 @@ from karp.lex_core.dtos import EntryDtoDict
 
 
 def test_can_create_entry_dto():
-    entry_dto = EntryDtoDict(entry={"field": "value"})
+    _entry_dto = EntryDtoDict(entry={"field": "value"})
